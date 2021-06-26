a328f84dd080c8c4d21e5811086656a66232aa9c
Jasenko Karovic <karovicm995@gmail.com>
2021-06-26 16:30:24 +0200

    style: Update type definition

--- vue3-date-picker.d.ts
+++ vue3-date-picker.d.ts
@@ -1,4 +1,7 @@
 import { DefineComponent, Plugin } from 'vue';
+import { RDatepickerProps } from './src/Vue3DatePicker/interfaces';
 
-declare const Vue3DatePicker: DefineComponent<{}, {}, any> & { install: Exclude<Plugin['install'], undefined> };
+declare const Vue3DatePicker: DefineComponent<RDatepickerProps, {}, any> & {
+    install: Exclude<Plugin['install'], undefined>;
+};
 export default Vue3DatePicker;
