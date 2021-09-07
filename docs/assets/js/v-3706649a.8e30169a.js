72626f5cc7d279c5f6b9c1efd8a1e73830c76735
Jasenko Karovic <karovicm995@gmail.com>
2021-09-07 18:14:02 +0200

    chore: Update docs

--- docs/assets/js/v-3706649a.8e30169a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[88],{1801:(e,t,a)=>{a.r(t),a.d(t,{data:()=>l});const l={key:"v-3706649a",path:"/404.html",title:"",lang:"en-US",frontmatter:{layout:"404"},excerpt:"",headers:[],filePathRelative:null,git:{}}},3881:(e,t,a)=>{a.r(t),a.d(t,{default:()=>l});const l={render:function(e,t){return null}}}}]);
