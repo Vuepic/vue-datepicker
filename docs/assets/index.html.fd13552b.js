61fa17849317ca343e90ad256211ed6e0776c719
Jasenko Karovic <karovicm995@gmail.com>
2021-12-20 16:21:34 +0100

    chore: Update documentation

---
+++ docs/assets/index.html.fd13552b.js
@@ -0,0 +1,1 @@
+const t={key:"v-241ec4c4",path:"/customization/theming/",title:"Theming",lang:"en-US",frontmatter:{title:"Theming",description:"Datepicker theme configuration for light and dark theme"},excerpt:"",headers:[],git:{updatedTime:1638711662e3},filePathRelative:"customization/theming/README.md"};export{t as data};
