7d0d384faa79f5f09afb80d0489472d980701f47
Jasenko Karovic <karovicm995@gmail.com>
2022-02-03 18:15:34 +0100

    chore: Test code organization
    - Add test case for multiDates

--- tests/unit/logic.spec.ts
+++ tests/unit/logic.spec.ts
@@ -16,18 +16,22 @@ const format = (date: Date): string => {
     return `Selected year is ${date.getFullYear()}`;
 };
 
+const mountDatepicker = async (props: any = {}): Promise<{ dp: VueWrapper<any>; menu: VueWrapper<any> }> => {
+    const dp = mount(Datepicker, { props });
+
+    dp.vm.openMenu();
+
+    await dp.vm.$nextTick();
+
+    const menu = dp.findComponent(DatepickerMenu);
+
+    return { dp, menu };
+};
+
 describe('Logic connection', () => {
     it('Should properly define initial values', async () => {
         const date = new Date();
-        const dp = mount(Datepicker, { props: { modelValue: date } });
-
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
-
-        await menu.vm.$nextTick();
+        const { menu } = await mountDatepicker({ modelValue: date });
 
         expect(menu.vm.month(0)).toEqual(date.getMonth());
         expect(menu.vm.year(0)).toEqual(date.getFullYear());
@@ -38,13 +42,7 @@ describe('Logic connection', () => {
     it('Should properly map initial values for range picker', async () => {
         const start = new Date();
         const end = addDays(start, 7);
-        const dp = mount(Datepicker, { props: { modelValue: [start, end], range: true } });
-
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
+        const { menu } = await mountDatepicker({ modelValue: [start, end], range: true });
 
         expect(menu.vm.hours).toHaveLength(2);
         expect(menu.vm.minutes).toHaveLength(2);
@@ -53,13 +51,7 @@ describe('Logic connection', () => {
 
     it('Should select date', async () => {
         const tomorrow = setSeconds(addDays(new Date(), 1), 0);
-        const dp = mount(Datepicker, { props: { modelValue: null } });
-
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu = dp.findComponent(DatepickerMenu);
+        const { dp, menu } = await mountDatepicker({ modelValue: null });
 
         const calendar = menu.findComponent(Calendar);
 
@@ -72,13 +64,8 @@ describe('Logic connection', () => {
     it('Should select range', async () => {
         const start = setSeconds(addDays(new Date(), 1), 0);
         const end = setSeconds(addDays(start, 7), 0);
-        const dp = mount(Datepicker, { props: { modelValue: null, range: true } });
+        const { menu } = await mountDatepicker({ modelValue: null, range: true });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const calendar = menu.findComponent(Calendar);
         calendar.vm.$emit('selectDate', { value: start, current: true });
         await calendar.vm.$nextTick();
@@ -96,13 +83,8 @@ describe('Logic connection', () => {
     it('Should select auto range', async () => {
         const start = setSeconds(new Date(), 0);
         const end = setSeconds(addDays(start, 7), 0);
-        const dp = mount(Datepicker, { props: { modelValue: null, range: true, autoRange: 7 } });
+        const { menu } = await mountDatepicker({ modelValue: null, range: true, autoRange: 7 });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const calendar = menu.findComponent(Calendar);
 
         calendar.vm.$emit('selectDate', { value: start, current: true });
@@ -116,13 +98,8 @@ describe('Logic connection', () => {
     it('Should update time', async () => {
         const val = 15;
         const date = new Date();
-        const dp = mount(Datepicker, { props: { modelValue: date } });
+        const { menu } = await mountDatepicker({ modelValue: date });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const timePicker = menu.findComponent(TimePicker);
         timePicker.vm.$emit('update:hours', val);
         await timePicker.vm.$nextTick();
@@ -137,13 +114,8 @@ describe('Logic connection', () => {
         const val = 15;
         const start = new Date();
         const end = addDays(new Date(), 7);
-        const dp = mount(Datepicker, { props: { modelValue: [start, end], range: true } });
+        const { menu } = await mountDatepicker({ modelValue: [start, end], range: true });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const timePicker = menu.findComponent(TimePicker);
         timePicker.vm.$emit('update:hours', [start.getHours(), val]);
         await timePicker.vm.$nextTick();
@@ -156,13 +128,8 @@ describe('Logic connection', () => {
 
     it('Should set month', async () => {
         const month = 0;
-        const dp = mount(Datepicker, { props: { modelValue: null, range: true } });
+        const { menu } = await mountDatepicker({ modelValue: null, range: true });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const monthYearInput = menu.findComponent(MonthYearInput);
         monthYearInput.vm.$emit('update:month', month);
 
@@ -171,13 +138,8 @@ describe('Logic connection', () => {
 
     it('Should set year', async () => {
         const year = 2022;
-        const dp = mount(Datepicker, { props: { modelValue: null, range: true } });
+        const { menu } = await mountDatepicker({ modelValue: null, range: true });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const monthYearInput = menu.findComponent(MonthYearInput);
         monthYearInput.vm.$emit('update:year', year);
 
@@ -187,13 +149,8 @@ describe('Logic connection', () => {
     it('Should format with custom function', async () => {
         const selected = new Date();
 
-        const dp = mount(Datepicker, { props: { modelValue: null, format } });
+        const { dp, menu } = await mountDatepicker({ modelValue: null, format });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const calendar = menu.findComponent(Calendar);
         calendar.vm.$emit('selectDate', { value: selected, current: true });
         await calendar.vm.$nextTick();
@@ -206,13 +163,8 @@ describe('Logic connection', () => {
     it('Should display preview format from function', async () => {
         const selected = new Date();
 
-        const dp = mount(Datepicker, { props: { modelValue: null, previewFormat: format } });
+        const { menu } = await mountDatepicker({ modelValue: null, previewFormat: format });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const calendar = menu.findComponent(Calendar);
         calendar.vm.$emit('selectDate', { value: selected, current: true });
         await calendar.vm.$nextTick();
@@ -225,13 +177,13 @@ describe('Logic connection', () => {
         const selected = new Date(0);
         // The day of the week (E) is locale-sensitive in Japanese.
         // Since epoch time zero (1970/1/1) was a Thursday, the 'Thu' must be localized to '木'.
-        const dp = mount(Datepicker, { props: { modelValue: null, format: 'E', locale: 'ja-JP', formatLocale: ja } });
+        const { dp, menu } = await mountDatepicker({
+            modelValue: null,
+            format: 'E',
+            locale: 'ja-JP',
+            formatLocale: ja,
+        });
 
-        dp.vm.openMenu();
-
-        await dp.vm.$nextTick();
-
-        const menu: VueWrapper<any> = dp.findComponent(DatepickerMenu);
         const calendar = menu.findComponent(Calendar);
         calendar.vm.$emit('selectDate', { value: selected, current: true });
         await calendar.vm.$nextTick();
@@ -239,5 +191,24 @@ describe('Logic connection', () => {
         await dp.vm.$nextTick();
 
         expect(dp.vm.inputValue).toEqual('木');
+    });
+
+    it('Should select multiple dates', async () => {
+        const dates = [new Date(), addDays(new Date(), 1), addDays(new Date(), 2), addDays(new Date(), 3)];
+        const { dp, menu } = await mountDatepicker({ modeValue: null, multiDates: true });
+
+        const calendar = menu.findComponent(Calendar);
+        for (const date of dates) {
+            calendar.vm.$emit('selectDate', { value: setSeconds(date, 0), current: true });
+            await calendar.vm.$nextTick();
+        }
+        expect(dp.vm.internalModelValue).toHaveLength(4);
+        expect(dp.vm.internalModelValue[0]).toEqual(setSeconds(dates[0], 0));
+
+        for (const date of dates) {
+            calendar.vm.$emit('selectDate', { value: setSeconds(date, 0), current: true });
+            await calendar.vm.$nextTick();
+        }
+        expect(dp.vm.internalModelValue).toBeNull();
     });
 });
