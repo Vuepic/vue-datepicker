bfa0692eae89823c4e95d1ce88e3f0ad721ba497
Jasenko Karovic <karovicm995@gmail.com>
2022-08-01 18:07:15 +0200

    chore: Update documentation

---
+++ docs/assets/index.html.898b768f.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-241ec4c4","path":"/customization/theming/","title":"Theming","lang":"en-US","frontmatter":{"title":"Theming","description":"Datepicker theme configuration for light and dark theme"},"excerpt":"","headers":[],"git":{"updatedTime":1659010904000},"filePathRelative":"customization/theming/README.md"}');export{e as data};
