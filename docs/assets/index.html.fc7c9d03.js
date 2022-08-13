93a142426b3e77ef66d60ebabe67592d2bc742b6
Jasenko Karovic <karovicm995@gmail.com>
2022-08-13 13:33:37 +0200

    chore: Update documentation

--- docs/assets/index.html.fc7c9d03.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-d446beac","path":"/customization/scss/","title":"SCSS","lang":"en-US","frontmatter":{"title":"SCSS","description":"Datepicker custom scss configuration"},"excerpt":"","headers":[],"git":{"updatedTime":1658422227000},"filePathRelative":"customization/scss/README.md"}');export{t as data};
