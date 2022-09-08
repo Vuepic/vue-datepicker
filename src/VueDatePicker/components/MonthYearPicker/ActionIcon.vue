7ade4c919159ac0539d5a4971675b05c1d8013e1
Jasenko Karovic <karovicm995@gmail.com>
2022-09-08 11:37:47 +0200

    style: Add `aria-disabled` on disabled nav button

--- src/VueDatePicker/components/MonthYearPicker/ActionIcon.vue
+++ src/VueDatePicker/components/MonthYearPicker/ActionIcon.vue
@@ -6,9 +6,11 @@
         tabindex="0"
         role="button"
         :aria-label="ariaLabel"
+        :aria-disabled="disabled"
         ref="elRef"
     >
-        <div class="dp__inner_nav">
+        <div class="dp__inner_nav" :class="{ dp__inner_nav_disabled: disabled }"
+        >
             <slot />
         </div>
     </div>
@@ -19,7 +21,10 @@
     import { onMounted, ref } from 'vue';
 
     const emit = defineEmits(['activate', 'setRef']);
-    defineProps({ ariaLabel: { type: String as PropType<string>, default: '' } });
+    defineProps({
+        ariaLabel: { type: String as PropType<string>, default: '' },
+        disabled: { type: Boolean as PropType<boolean>, default: false },
+    });
     const elRef = ref(null);
 
     onMounted(() => emit('setRef', elRef));
