48e3e96c0a15748056d9cf6b8eb96757e60fa5e0
Alejandro Visciglio <alejandro@Alejandros-MacBook-Pro.local>
2022-09-22 13:06:27 -0300

    refactor(type definition): export interfaces in declaration file

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
 import { Locale } from 'date-fns';
 import { ComponentOptionsMixin, ComputedOptions, DefineComponent, MethodOptions } from 'vue';
 
-type EmitEvents =
+export type EmitEvents =
     | 'update:modelValue'
     | 'textSubmit'
     | 'closed'
@@ -16,7 +16,7 @@ type EmitEvents =
     | 'updateMonthYear'
     | 'invalid-select';
 
-interface VueDatePicker {
+export interface VueDatePicker {
     uid?: string;
     name?: string;
     is24?: boolean;
@@ -233,7 +233,7 @@ interface VueDatePicker {
     ignoreTimeValidation?: boolean;
 }
 
-interface PublicMethods extends MethodOptions {
+export interface PublicMethods extends MethodOptions {
     selectDate: () => void;
     closeMenu: () => void;
     openMenu: () => void;
