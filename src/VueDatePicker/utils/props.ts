7ee5c4a50256feddde754ba66e95cba5d3950b54
pochern <pchernomaz@transperfect.com>
2023-05-26 18:38:00 -0400

    feat: add closeOnClearValue prop

--- src/VueDatePicker/utils/props.ts
+++ src/VueDatePicker/utils/props.ts
@@ -160,6 +160,7 @@ export const AllProps = {
     sixWeeks: { type: [Boolean, String] as PropType<boolean | SixWeekMode>, default: false },
     actionRow: { type: Object as PropType<Partial<ActionRowData>>, default: () => ({}) },
     allowPreventDefault: { type: Boolean as PropType<boolean>, default: false },
+    closeOnClearValue: { type: Boolean as PropType<boolean>, default: true },
 };
 
 export type AllPropsType = ExtractPropTypes<typeof AllProps>;
