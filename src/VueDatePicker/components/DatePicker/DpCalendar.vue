Vuepic/vue-datepicker

commit c9b602d8d20cc08c45a25c4753ac696b7cb42880

feat: Add support for drag and select on `multi-dates` (resolves #780)

src/VueDatePicker/components/DatePicker/DpCalendar.vue
@@ -31,6 +31,7 @@
                     class="dp__calendar"
                     role="rowgroup"
                     :aria-label="defaultedAriaLabels?.calendarDays || undefined"
+                    @mouseleave="isMouseDown = false"
                 >
                     <div v-for="(week, weekInd) in calendarWeeks" :key="weekInd" class="dp__calendar_row" role="row">
                         <div v-if="weekNumbers" role="gridcell" class="dp__calendar_item dp__week_num">
@@ -60,6 +61,8 @@
                             @keydown.space="$emit('handle-space', dayVal)"
                             @mouseenter="onMouseOver(dayVal, weekInd, dayInd)"
                             @mouseleave="onMouseLeave(dayVal)"
+                            @mousedown="onMouseDown(dayVal)"
+                            @mouseup="isMouseDown = false"
                         >
                             <div class="dp__cell_inner" :class="dayVal.classData">
                                 <slot
@@ -165,6 +168,7 @@
         defaultedAriaLabels,
         defaultedMultiCalendars,
         defaultedWeekNumbers,
+        defaultedMultiDates,
     } = useDefaults(props);
 
     const showMakerTooltip = ref<Date | null>(null);
@@ -180,6 +184,7 @@
     const touch = ref({ startX: 0, endX: 0, startY: 0, endY: 0 });
     const activeTooltip = ref<HTMLElement[]>([]);
     const tpArrowStyle = ref<{ left?: string; right?: string }>({ left: '50%' });
+    const isMouseDown = ref(false);
 
     const calendarWeeks = computed(() => {
         if (props.calendar) return props.calendar(props.mappedDates);
@@ -254,6 +259,9 @@
     };
 
     const onMouseOver = async (day: UnwrapRef<ICalendarDay>, weekInd: number, dayInd: number): Promise<void> => {
+        if (isMouseDown.value && defaultedMultiDates.value.enabled && defaultedMultiDates.value.dragSelect) {
+            return emit('select-date', day);
+        }
         emit('set-hover-date', day);
         if (day.marker?.tooltip?.length) {
             const el = unrefElement(dayRefs.value[weekInd][dayInd]);
@@ -361,12 +369,23 @@
     };
 
     const onDateSelect = (ev: Event, dayVal: ICalendarDay) => {
-        checkStopPropagation(ev, defaultedConfig.value);
-        emit('select-date', dayVal);
+        if (!defaultedMultiDates.value.enabled) {
+            checkStopPropagation(ev, defaultedConfig.value);
+            emit('select-date', dayVal);
+        }
     };
 
     const onTpClick = (ev: Event) => {
         checkStopPropagation(ev, defaultedConfig.value);
+    };
+
+    const onMouseDown = (day: UnwrapRef<ICalendarDay>) => {
+        if (defaultedMultiDates.value.enabled && defaultedMultiDates.value.dragSelect) {
+            isMouseDown.value = true;
+            emit('select-date', day);
+        } else {
+            emit('select-date', day);
+        }
     };
 
     defineExpose({ triggerTransition });

index.d.ts
@@ -207,7 +207,7 @@ export interface VueDatePickerProps {
     monthChangeOnArrows?: boolean;
     formatLocale?: Locale;
     autocomplete?: string;
-    multiDates?: boolean;
+    multiDates?: boolean | { limit?: number | string; dragSelect?: boolean };
     presetDates?: {
         label: string;
         value: Date[] | string[] | string | Date;
@@ -235,6 +235,9 @@ export interface VueDatePickerProps {
      */
     fixedEnd?: boolean;
     utc?: boolean | 'preserve';
+    /**
+     * @deprecated
+     */
     multiDatesLimit?: number | string;
     reverseYears?: boolean;
     weekPicker?: boolean;
@@ -286,7 +289,9 @@ export interface VueDatePickerProps {
      */
     noDisabledRange?: boolean;
     sixWeeks?: boolean | 'append' | 'prepend' | 'center' | 'fair';
-    timezone?: string | { timezone?: string; exactMatch?: boolean; dateInTz?: string; emitTimezone?: string };
+    timezone?:
+        | string
+        | { timezone?: string; exactMatch?: boolean; dateInTz?: string; emitTimezone?: string; convertModel?: boolean };
     /**
      * @deprecated
      */

src/VueDatePicker/VueDatePicker.vue
@@ -157,8 +157,14 @@
     const { setMenuFocused, setShiftKey } = useState();
     const { clearArrowNav } = useArrowNavigation();
     const { validateDate, isValidTime } = useValidation(props);
-    const { defaultedTransitions, defaultedTextInput, defaultedInline, defaultedConfig, defaultedRange } =
-        useDefaults(props);
+    const {
+        defaultedTransitions,
+        defaultedTextInput,
+        defaultedInline,
+        defaultedConfig,
+        defaultedRange,
+        defaultedMultiDates,
+    } = useDefaults(props);
     const { menuTransition, showTransition } = useTransitions(defaultedTransitions);
 
     onMounted(() => {
@@ -334,7 +340,7 @@
         if (!date) return true;
         if (!Array.isArray(date) && validateDate(date)) return true;
         if (Array.isArray(date)) {
-            if (props.multiDates) return true;
+            if (defaultedMultiDates.value.enabled) return true;
 
             if (date.length === 2 && validateDate(date[0]) && validateDate(date[1])) {
                 return true;

src/VueDatePicker/components/ActionRow.vue
@@ -101,6 +101,7 @@
         defaultedTextInput,
         defaultedInline,
         defaultedRange,
+        defaultedMultiDates,
         getDefaultPattern,
     } = useDefaults(props);
     const { isTimeValid, isMonthValid } = useValidation(props);
@@ -186,7 +187,7 @@
                 if (props.internalModelValue.length === 2 && props.internalModelValue[1]) {
                     return formatRangeDate();
                 }
-                if (props.multiDates) {
+                if (defaultedMultiDates.value.enabled) {
                     return props.internalModelValue.map((date) => `${formatDatePreview(date)}`);
                 }
                 if (props.modelAuto) {
@@ -199,7 +200,7 @@
         return handleCustomPreviewFormat();
     });
 
-    const dateSeparator = () => (props.multiDates ? '; ' : ' - ');
+    const dateSeparator = () => (defaultedMultiDates.value.enabled ? '; ' : ' - ');
 
     const formatValue = computed(() =>
         !Array.isArray(previewValue.value) ? previewValue.value : previewValue.value.join(dateSeparator()),

src/VueDatePicker/components/DatePicker/date-picker.ts
@@ -48,7 +48,8 @@ export const useDatePicker = (
     const lastScrollTime = ref(new Date());
 
     const { modelValue, calendars, time } = useModel(props, emit);
-    const { defaultedMultiCalendars, defaultedStartTime, defaultedRange, defaultedTz, propDates } = useDefaults(props);
+    const { defaultedMultiCalendars, defaultedStartTime, defaultedRange, defaultedTz, propDates, defaultedMultiDates } =
+        useDefaults(props);
     const { validateMonthYearInRange, isDisabled, isDateRangeAllowed, checkMinMaxRange } = useValidation(props);
     const { updateTimeValues, getSetDateTime, setTime, assignStartTime, validateTime, disabledTimesConfig } =
         useTimePickerUtils(props, time, modelValue, updateFlow);
@@ -199,11 +200,11 @@ export const useDatePicker = (
 
     // Assign range values, or in case of multiDates, set
     const assignExistingMulti = (dates: Date[], fromMount: boolean) => {
-        if ((defaultedRange.value.enabled || props.weekPicker) && !props.multiDates) {
+        if ((defaultedRange.value.enabled || props.weekPicker) && !defaultedMultiDates.value.enabled) {
             return assignRangeValue(dates, fromMount);
         }
 
-        if (props.multiDates && fromMount) {
+        if (defaultedMultiDates.value.enabled && fromMount) {
             const lastEntry = dates[dates.length - 1];
             return assignSingleValue(lastEntry, fromMount);
         }
@@ -404,8 +405,8 @@ export const useDatePicker = (
     const handleSingleDateSelect = (day: ICalendarDay) => {
         const date = setDateTime(getDate(day.value), time.hours as number, time.minutes as number, getSecondsValue());
         emit('date-update', date);
-        if (props.multiDates) {
-            handleMultiDatesSelect(date, modelValue, props.multiDatesLimit);
+        if (defaultedMultiDates.value.enabled) {
+            handleMultiDatesSelect(date, modelValue, defaultedMultiDates.value.limit);
         } else {
             modelValue.value = date;
         }
@@ -554,7 +555,7 @@ export const useDatePicker = (
 
         if (!defaultedRange.value.enabled) return handleSingleDateSelect(day);
 
-        if (isNumberArray(time.hours) && isNumberArray(time.minutes) && !props.multiDates) {
+        if (isNumberArray(time.hours) && isNumberArray(time.minutes) && !defaultedMultiDates.value.enabled) {
             handleRangeDatesSelect(day, isNext);
             postRangeSelect();
         }
@@ -608,7 +609,7 @@ export const useDatePicker = (
 
     const handleTimeUpdate = () => {
         if (Array.isArray(modelValue.value)) {
-            if (props.multiDates) {
+            if (defaultedMultiDates.value.enabled) {
                 const lastEntry = multiDatesLast();
                 modelValue.value[modelValue.value.length - 1] = getSetDateTime(lastEntry as Date);
             } else {

src/VueDatePicker/components/DatepickerInput.vue
@@ -118,6 +118,7 @@
         defaultedInline,
         defaultedConfig,
         defaultedRange,
+        defaultedMultiDates,
         getDefaultPattern,
         getDefaultStartTime,
     } = useDefaults(props);
@@ -186,7 +187,7 @@
     const parseInput = (value: string) => {
         if (defaultedRange.value.enabled) {
             handleRangeTextInput(value);
-        } else if (props.multiDates) {
+        } else if (defaultedMultiDates.value.enabled) {
             const dates = value.split(`;`);
             parsedDate.value = dates.map((val) => parser(val.trim())).filter((val) => val);
         } else {

src/VueDatePicker/components/MonthPicker/month-picker.ts
@@ -36,6 +36,7 @@ export const useMonthPicker = (props: PickerBasePropsType, emit: VueEmit) => {
         propDates,
         defaultedTz,
         defaultedFilters,
+        defaultedMultiDates,
     } = useDefaults(props);
 
     const { modelValue, year, month: instanceMonth, calendars } = useModel(props, emit);
@@ -163,14 +164,14 @@ export const useMonthPicker = (props: PickerBasePropsType, emit: VueEmit) => {
     };
 
     const selectMultiMonths = (month: number, instance: number) => {
-        handleMultiDatesSelect(monthToDate(month, instance), modelValue, props.multiDatesLimit);
+        handleMultiDatesSelect(monthToDate(month, instance), modelValue, defaultedMultiDates.value.limit);
         emit('auto-apply', true);
     };
 
     const selectMonth = (month: number, instance: number) => {
         calendars.value[instance].month = month;
         emitMonthYearUpdate(instance, calendars.value[instance].year, month);
-        if (props.multiDates) return selectMultiMonths(month, instance);
+        if (defaultedMultiDates.value.enabled) return selectMultiMonths(month, instance);
         if (defaultedRange.value.enabled) return selectRangedMonth(month, instance);
         return selectSingleMonth(month, instance);
     };

src/VueDatePicker/components/QuarterPicker/quarter-picker.ts
@@ -30,6 +30,7 @@ export const useQuarterPicker = (props: PickerBasePropsType, emit: VueEmit) => {
         defaultedRange,
         propDates,
         defaultedFilters,
+        defaultedMultiDates,
     } = useDefaults(props);
     const { modelValue, year, month, calendars } = useModel(props, emit);
     const { isDisabled: isDateDisabled } = useValidation(props);
@@ -105,7 +106,7 @@ export const useQuarterPicker = (props: PickerBasePropsType, emit: VueEmit) => {
     });
 
     const selectMultiQuarters = (date: Date) => {
-        handleMultiDatesSelect(date, modelValue, props.multiDatesLimit);
+        handleMultiDatesSelect(date, modelValue, defaultedMultiDates.value.limit);
         emit('auto-apply', true);
     };
 
@@ -123,7 +124,7 @@ export const useQuarterPicker = (props: PickerBasePropsType, emit: VueEmit) => {
         if (disabled) return;
         calendars.value[instance].month = getMonth(endOfQuarter(date));
 
-        if (props.multiDates) return selectMultiQuarters(date);
+        if (defaultedMultiDates.value.enabled) return selectMultiQuarters(date);
         if (defaultedRange.value.enabled) return selectRangedQuarter(date);
         return selectSingleQuarter(date);
     };

commit 51fe676d3147a4d254d122616b7a8d81b938d29a

fix: Resolve `range` select issue introduced in c9b602d8d20cc08c45a25c4753ac696b7cb42880

src/VueDatePicker/components/DatePicker/DpCalendar.vue
@@ -387,7 +387,7 @@
         if (defaultedMultiDates.value.enabled && defaultedMultiDates.value.dragSelect) {
             isMouseDown.value = true;
             emit('select-date', day);
-        } else {
+        } else if (defaultedMultiDates.value.enabled) {
             emit('select-date', day);
         }
     };