36ed4a2953a49bf3993276d02a631e93242c6d4a
Jasenko Karovic <karovicm995@gmail.com>
2024-12-28 13:29:03 +0100

    feat: Add `AllProps` export (resolves #1032)

--- src/entry.ts
+++ src/entry.ts
@@ -1,7 +1,10 @@
 import component, * as namedExports from './entry.esm';
+import { AllProps } from '@/props.ts';
 
 Object.entries(namedExports).forEach(([exportName, exported]) => {
     if (exportName !== 'default') component[exportName] = exported;
 });
 
 export default component;
+
+export { AllProps };
