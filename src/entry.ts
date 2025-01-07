645d3b8cb200c412af84ba2682237724b1205dfb
Jasenko Karovic <karovicm995@gmail.com>
2025-01-07 18:42:12 +0100

    revert: Remove AllProps export

--- src/entry.ts
+++ src/entry.ts
@@ -1,10 +1,7 @@
 import component, * as namedExports from './entry.esm';
-import { AllProps } from '@/props.ts';
 
 Object.entries(namedExports).forEach(([exportName, exported]) => {
     if (exportName !== 'default') component[exportName] = exported;
 });
 
 export default component;
-
-export { AllProps };
