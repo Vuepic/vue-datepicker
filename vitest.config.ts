3a681d21c5b6f514363abb79feabd3996bb21f31
Jasenko Karovic <karovicm995@gmail.com>
2025-03-29 20:21:47 +0100

    refactor: Update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'node:url';
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import { mergeConfig, defineConfig, configDefaults, defaultExclude } from 'vitest/config';
 import viteConfig from './vite.config';
 import { resolve } from 'path';
 
@@ -7,35 +7,13 @@ export default mergeConfig(
     viteConfig,
     defineConfig({
         test: {
-            globals: true,
             environment: 'jsdom',
             exclude: [...configDefaults.exclude, 'e2e/*'],
             root: fileURLToPath(new URL('./', import.meta.url)),
             coverage: {
                 reporter: ['lcov'],
-                reportsDirectory: resolve(__dirname, 'coverage'),
-                exclude: [
-                    'coverage/**',
-                    'dist/**',
-                    '**/[.]**',
-                    'packages/*/ test?(s)/**',
-                    '**/*.d.ts',
-                    '**/ virtual:*',
-                    '**/__x00__*',
-                    '**/\x00*',
-                    'cypress/**',
-                    'test?(s)/**',
-                    'test?(-*).?(c|m)[jt] s?(x)',
-                    '**/*{.,-}{test,spec}?(-d).?(c|m)[jt] s?(x)',
-                    '**/__tests__/**',
-                    '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-                    '**/ vitest.{workspace,projects}.[jt] s?(on)',
-                    '**/.{eslint,mocha,prettier}rc.{?(c|m) js,yml}',
-                    'dev/**',
-                    'src/entry.ts',
-                    'src/entry.esm.ts',
-                    'docs/**',
-                ],
+                reportsDirectory: resolve(import.meta.dirname, 'coverage'),
+                exclude: [...defaultExclude, 'dev/**', 'src/entry.ts', 'src/entry.esm.ts', 'docs/**'],
             },
         },
     }),
