87808e6dae1694918d2b53387dabcf1ebe8bc005
Amirkian2023 <amirkianvash2023@gmail.com>
2025-08-19 20:42:10 +0330

    git commit -m "chore(demo): remove space in dev/serve.vue

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="wrapper">
-        <Datepicker  v-model="selectedDate" placeholder="Select Date" />
-    </div>
+  <div class="wrapper">
+    <Datepicker v-model="selectedDate" placeholder="Select Date" />
+  </div>
 </template>
 
 <script lang="ts" setup>
-    import { ref } from 'vue';
-    import Datepicker from '../src/VueDatePicker/VueDatePicker.vue';
+import { ref } from 'vue';
+import Datepicker from '../src/VueDatePicker/VueDatePicker.vue';
 
-    const selectedDate = ref();
+const selectedDate = ref();
 </script>
 
 <style lang="scss">
-    .wrapper {
-        padding: 200px;
-    }
+.wrapper {
+  padding: 200px;
+}
 </style>
