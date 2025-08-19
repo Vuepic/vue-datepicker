5c36e35ce27a4d82133ffe1fdc9489f95db131b6
Amirkian2023 <amirkianvash2023@gmail.com>
2025-08-19 19:44:57 +0330

    fix: correct padding when clearable=false

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wrapper">
-        <Datepicker v-model="selectedDate" placeholder="Select Date" />
+        <Datepicker  v-model="selectedDate" placeholder="Select Date" />
     </div>
 </template>
 
